bbbf44e1c953e6d49c5853b02e5388c85a09d70c
AnyIons <anyions@hotmail.com>
2024-10-21 14:01:02 +0800

    feat(*): lambda entrypoint

--- awstt/lambda.py
+++ awstt/lambda.py
@@ -1,2 +1,28 @@
+import logging
+
+import executor
+from config import init_config
+
+
+logger = logging.getLogger(__name__)
+
+
 def execute(evt: any, _):
-    pass
+    """
+    Lambda input uses dict in format of json configure.
+    """
+
+    if evt.get("action", None) is None:
+        return
+
+    config = init_config(
+        dict(
+            action=evt.get("action", None),
+            tags=evt.get("tags", []),
+            filters=evt.get("filters", []),
+            resources=evt.get("resources", []),
+            force=evt.get("force", False),
+        )
+    )
+
+    executor.run(config)
